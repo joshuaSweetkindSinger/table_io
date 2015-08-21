48d94d40e163bcf3ef4580cc605664918ce07597
joshua sweetkind-singer <sweetkindsinger@gmail.com>
2015-08-21 15:09:21 -0700

    small change

--- test/do_tests.rb
+++ test/do_tests.rb
@@ -98,7 +98,10 @@ module TableIo
       def run
         with_run_scaffold do
           TableIo::source('test1.csv') >>
-            Delimited::Reader.new >> Delimited::Writer.new >> TableIo::sink('test3.csv')
+            Delimited::Reader.new >>
+            Delimited::Writer.new >>
+            TableIo::sink('test3.csv')
+          
           files_identical?('test1_correct_output.csv', 'test3.csv')
         end
       end
