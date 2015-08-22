600726de1b7d734e05eb36817175dc2363499694
joshua sweetkind-singer <sweetkindsinger@gmail.com>
2015-08-22 09:30:00 -0700

    add documentation

--- table_io.rb
+++ table_io.rb
@@ -9,31 +9,51 @@
 # a table from one format to another.
 #
 # There are three main types of objects: Reader, Writer, and Record.
+#
+# RECORD
 # A Record object is a generic representation of a record in a table: it maps column names to their values.
-# A Reader object is initialized from a stream opened to a table and turns it into an iterator of Record objects, also called a "record stream".
-# A Writer object is initialized from a record stream and turns it back into an iterator of characters representing the table.
 #
-# If r is a Reader, then r is an iterator of the records of its source table,
-# and r.each is an enumeration of the same. Also, r.columns is an
-# array of strings representing the names of the table's columns, in column-order. Note that the first element
-# of the enumeration is *not* the column header. You only get that via r.columns.
-
-# If w is a Writer, then w is an iterator of the character sequence that represents the source records,
-# and w.each is an enumeration of the same. Since this is a text representation of the table,
-# the initial characters in the sequence *will* be the table's
-# column header, which defines the column names and column order of the table. If r is a Reader, then both r
-# and r.each are suitable initializers for a Writer object. The former is preferred.
+# READER
+# A Reader object is initialized from a character stream (opened from a table) and turns it into
+# an iterator of Record objects, also called a "record stream". A particular Reader class knows how
+# to parse the character stream from a particular table format. For example, a Delimited::Reader knows
+# how to parse the records of a delimited table, such as a csv table.
 #
+# If r is a Reader receiving input from the character stream of a table t,
+# then r is an iterator of the records of t, and r.each is an enumeration of the same.
+# Also, r.columns is an array of strings representing the names of t's columns, in column-order.
+# Note that the first element of the enumeration is *not* the column header.
+# You only get that via r.columns.
+#
+# WRITER
+# A Writer object is initialized from a record stream and turns it back into
+# an iterator of characters representing a table. A particular Writer class knows how to write
+# a character stream in a particular table format. For example, a Delimited::Writer knows how
+# to write the records of a delimited table, such as a csv table.
+#
+# # If w is a Writer receiving input from a Reader r, which in turn receives input from a table t,
+# then w is an iterator of the character sequence that represents t translated into w's format,
+# and w.each is an enumeration of the same.
+#    Since this is a text representation of the table, the initial characters in the sequence *will*
+# be the table's column header, which defines the column names and column order of the table.
+#
+# * * *
+#
+# PIPES
 # It can be useful to chain together readers, writers, and other stream processors in order to transform
 # an initial input file into an altered, filtered version of the file in another format, with each processor
 # in the chain performing a single task. Such a chain is called a "pipe", and the readers and writers defined
 # in this project support piping via the >> operator. See TestSameFormatUsingPipe in do_tests.rb for an example.
 
-# Currently this file implements readers and writers for delimited files, such as csv or tab-delimited, as well
+# Currently this project implements readers and writers for delimited files, such as csv or tab-delimited, as well
 # as readers and writers for JSON and XML formats. These latter two are mainly for show, to prove that the architecture
 # is extensible. It is unlikely one would really want to use these formats in real life.
 #
-# The top-level instantiable classes are DelimitedReader, DelimitedWriter, JsonReader, JsonWriter, XmlReader, and XmlWriter.
+# The top-level instantiable classes are DelimitedReader, DelimitedWriter,
+# JsonReader, JsonWriter, XmlReader, and XmlWriter.
+#
+# This file just implements the base classes that create the infrastructure. The instantiable classes
+# are in their own sub-directories
 
 module TableIo
   # This module contains helper methods common to many of the classes used in the project.
